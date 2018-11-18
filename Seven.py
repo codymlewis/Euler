7540de69aeabfe021c1b0a6de842562f1eacb246
Cody Lewis <luxdotsugi@gmail.com>
2018-11-18 15:28:28 +1100

    Solution to problem 7

---
+++ Seven.py
@@ -0,0 +1,32 @@
+import math
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+that the 6th prime is 13.
+
+What is the 10 001st prime number?
+'''
+
+
+def prime(n):
+    # could cheat with SymPy, but that is not fun.
+    i = 2
+    prime_count = 0
+    while True:
+        if is_prime(i):
+            prime_count += 1
+        if prime_count == n:
+            return i
+        i += 1
+
+
+def is_prime(n):
+    for i in range(2, int(math.ceil(math.sqrt(n)))):
+        if (n % i) == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    LIMIT = 10_001
+    print("Started finding number...")
+    print(f"The {LIMIT}th prime is {prime(LIMIT)}")
